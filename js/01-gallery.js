fe533d75b4dbb473730013ad454c292d24925c6d
Dariaaal <1406dariaal@gmail.com>
2023-01-06 20:22:10 +0200

    half of task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -9,10 +9,28 @@ const imagesMarkup = createImagesMarkup(galleryItems);
 galleryList.insertAdjacentHTML('beforeend', imagesMarkup);
 
 function createImagesMarkup(galleryItems){
-    return galleryItems.map(({preview, description})=>{
-        return `<a class='gallery__link' href='#'>
+    return galleryItems.map(({preview, original, description})=>{
+        return `<a class='gallery__link' href='${original}'>
         <img  class='gallery__image' src ='${preview}' alt ='${description}'>
         </a>`;}).join('');
+}
+
+galleryList.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(e){
+    e.preventDefault();
+
+    if(e.target.nodeName !== 'IMG'){
+        return
+    }
+
+const instance = basicLightbox.create(`
+    <div class="modal">
+    <img src='${e.target.dataset.source}'>
+    </div>
+`)
+
+instance.show()
 }
 
 
